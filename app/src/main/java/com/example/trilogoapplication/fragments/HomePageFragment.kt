949b8c696d6a73dd87919e47ff7461ee432aa864
Joe02 <Joe02araujo@gmail.com>
2021-02-08 20:07:07 -0300

    Criado layout para tela inicial.

--- app/src/main/java/com/example/trilogoapplication/fragments/HomePageFragment.kt
+++ app/src/main/java/com/example/trilogoapplication/fragments/HomePageFragment.kt
@@ -4,15 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.trilogoapplication.R
+import com.example.trilogoapplication.databinding.FragmentHomePageBinding
 
 class HomePageFragment: Fragment() {
+
+    private lateinit var homeBinding: FragmentHomePageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        homeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_page, container, false)
+
+        setViewClickListeners()
+
+        return homeBinding.root
+    }
+
+    private fun setViewClickListeners() {
+        homeBinding.searchIcon.setOnClickListener {
+            ///TODO Adicionar navegação para tela de pesquisa.
+        }
+
+        homeBinding.floatingActionButton.setOnClickListener {
+            ///TODO Adicionar request.
+        }
     }
 }
