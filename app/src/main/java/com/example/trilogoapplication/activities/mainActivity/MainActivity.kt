163b6666f2aedc1d6644dde5e394ac1358e84cfa
Joe02 <Joe02araujo@gmail.com>
2021-02-08 18:36:02 -0300

    First commit.

---
+++ app/src/main/java/com/example/trilogoapplication/activities/mainActivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trilogoapplication.activities.mainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trilogoapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
